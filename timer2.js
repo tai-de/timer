9856927d2b3e860dd8ef6379ff1aa70dde25f0ef
Tai <Tai.Delisle@telus.com>
2022-11-23 10:43:32 -0700

    fix timer2

--- timer2.js
+++ timer2.js
@@ -3,24 +3,21 @@ const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 
-const numbers = ['\u0030','\u0031','\u0032','\u0033','\u0034','\u0035','\u0036','\u0037','\u0038','\u0039']
-
 stdin.on('data', (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
-    console.log(`\nThank you for using me, bye bye!`)
+    console.log(`\nThank you for using me, bye bye!`);
     process.exit();
   }
   // if b is pressed
-  if (key === '\u0062') {
+  if (key === 'b') {
     process.stdout.write('\r\u0007');
   }
   // if 1-9 is pressed
-  if (key >= '\u0031' && key <= '\u0039') {
-    timer = numbers.indexOf(key);
-    console.log(`setting timer for ${timer} seconds`)
+  if (key >= '1' && key <= '9') {
+    console.log(`setting timer for ${key} seconds`);
     setTimeout(() => {
       process.stdout.write('\r\u0007');
-    }, Number(timer) * 1000);
+    }, Number(key) * 1000);
   }
 });
